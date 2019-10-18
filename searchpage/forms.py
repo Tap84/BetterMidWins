59b0f87ceb908901ce1dbf28f1911e6ec2ca2c97
Tap84 <43452884+Tap84@users.noreply.github.com>
2019-10-18 15:46:10 -0400

    Vastly Simplified process, search bar now redirects to analyze w/ name

--- searchpage/forms.py
+++ searchpage/forms.py
@@ -1,4 +1,4 @@
 from django import forms
 
 class SearchForm(forms.Form):
-    search = forms.CharField(label = "Summoner Name", max_length=20, required=False)
+    summ_name = forms.CharField(label = "Summoner Name", max_length=20, required=True)
--- searchpage/views.py
+++ searchpage/views.py
@@ -1,7 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 import cassiopeia as cass
-import random
 from .forms import SearchForm
 from pathlib import Path
 
@@ -9,28 +8,9 @@ from pathlib import Path
 with open('apikey.txt') as f:
     cass.set_riot_api_key(f.read().strip())
 cass.set_default_region("NA")
-champions = cass.get_champions()
+#champions = cass.get_champions()
 
 # Create your views here.
 def search(request):
-    summ_name = 'kingtylerp'
-    summoner = cass.get_summoner(name=summ_name)
-    if request.method == "POST":
-        form = SearchForm(request.POST)
-        if form.is_valid():
-            
-            summ_name=form.cleaned_data['search']
-            #print(summ_name)
-        
-        summoner = cass.get_summoner(name=summ_name)
-        print(f"{summoner.name} is a level {summoner.level} summoner.")
-
-    
-    
-    random_champion = random.choice(champions)
-    
-    print("He enjoys playing champions such as {name}.".format(name=random_champion.name))
-
-
-    return render(request, 'searchpage/searchpage.html',{'current_name':summ_name, 'summoner':summoner})
+    return render(request, 'searchpage/searchpage.html')
 
