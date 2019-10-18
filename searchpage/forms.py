2cd8ca47ba4da50bbbd903d250dcee29c370863d
Tap84 <43452884+Tap84@users.noreply.github.com>
2019-10-18 14:33:45 -0400

    Searchpage exists, needs to be updated but functional

--- searchpage/forms.py
+++ searchpage/forms.py
@@ -1,4 +1,4 @@
 from django import forms
 
 class SearchForm(forms.Form):
-    summoner_name = forms.CharField(label = "Summoner Name", max_length=20, required=False, initial = 'test')
+    search = forms.CharField(label = "Summoner Name", max_length=20, required=False)
--- searchpage/views.py
+++ searchpage/views.py
@@ -3,30 +3,34 @@ from django.http import HttpResponse
 import cassiopeia as cass
 import random
 from .forms import SearchForm
+from pathlib import Path
+
+
+with open('apikey.txt') as f:
+    cass.set_riot_api_key(f.read().strip())
+cass.set_default_region("NA")
+champions = cass.get_champions()
 
 # Create your views here.
 def search(request):
-    summ_name = "test"
+    summ_name = 'kingtylerp'
+    summoner = cass.get_summoner(name=summ_name)
     if request.method == "POST":
         form = SearchForm(request.POST)
-        
-        print(form.is_valid())
         if form.is_valid():
-            summ_name=form.clean()
-            print(summ_name)
-        cass.set_riot_api_key("RGAPI-a39f9032-605b-4241-9294-490ca8bd42eb")  # This overrides the value set in your configuration/settings.
-        cass.set_default_region("NA")
-
+            
+            summ_name=form.cleaned_data['search']
+            #print(summ_name)
+        
         summoner = cass.get_summoner(name=summ_name)
-        print("{name} is a level {level} summoner on the {region} server.".format(name=summoner.name,
-                                                                                level=summoner.level,
-                                                                                region=summoner.region))
+        print(f"{summoner.name} is a level {summoner.level} summoner.")
 
-    #champions = cass.get_champions()
-    #print(champions)
-    #random_champion = random.choice(champions)
-    #print("He enjoys playing champions such as {name}.".format(name=random_champion.name))
+    
+    
+    random_champion = random.choice(champions)
+    
+    print("He enjoys playing champions such as {name}.".format(name=random_champion.name))
 
 
-    return render(request, 'searchpage/searchpage.html',{'current_name':summ_name})
+    return render(request, 'searchpage/searchpage.html',{'current_name':summ_name, 'summoner':summoner})
 
