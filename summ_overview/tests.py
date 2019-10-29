a0769d65c1d7456e4aefcf97f6bf5535646bd8ff
Tap84 <43452884+Tap84@users.noreply.github.com>
2019-10-29 14:39:53 -0400

    Trying to make tests work, basic format seems correct but has errors

--- summ_overview/tests.py
+++ summ_overview/tests.py
@@ -1,3 +1,14 @@
-from django.test import TestCase
+from django.test import TestCase, RequestFactory
+from summ_overview.views import analyze
+
 
 # Create your tests here.
+class SearchFormEmpty(TestCase):
+    def setup(self):
+        self.factory = RequestFactory()
+    def test_empty(self):
+        request = self.factory.get("/analyze/?summ_name=")
+        response = analyze(request)
+        
+        self.assertEqual(response,"Summoner doesnt exist")
+    
